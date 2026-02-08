fd894c32bde3050aa4c54a7e62f1b3eb18d99d92
sanket7474 <sanketmaske10@gmail.com>
2026-02-08 15:40:52 +0530

    first commit base project

---
+++ src/main/java/com/example/airBnBClone/AirBnBCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.airBnBClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBnBCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBnBCloneApplication.class, args);
+	}
+
+}
